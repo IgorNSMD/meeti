60600f02be0afe916a12718a527b5b773de4f0d0
IgorNSMD <insanmartind@gmail.com>
2023-03-10 16:51:10 -0300

    Capitulo 287

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const homeController = require('../controllers/homeController');
 const usuariosController = require('../controllers/usuariosController');
 const authController = require('../controllers/authController');
+const adminController = require('../controllers/adminController');
+const gruposController = require('../controllers/gruposController');
 
 module.exports = function() {
     router.get('/', homeController.home )
@@ -16,6 +18,18 @@ module.exports = function() {
     // Iniciar sesion
     router.get('/iniciar-sesion', usuariosController.formIniciarSesion);
     router.post('/iniciar-sesion', authController.autenticarUsuario);
+
+    /** Panel de administración */
+    router.get('/administracion',  
+        authController.usuarioAutenticado,
+        adminController.panelAdministracion 
+    );
+
+    /** Nuevos Grupos */
+    router.get('/nuevo-grupo',
+        authController.usuarioAutenticado,
+        gruposController.formNuevoGrupo
+    );    
 
     return router;
 
