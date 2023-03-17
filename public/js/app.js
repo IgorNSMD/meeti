3a45a784258b81c223292fa63ae9c69d29e2c332
IgorNSMD <insanmartind@gmail.com>
2023-03-17 20:55:16 -0300

    Capitulo 308

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,59 @@
-var map = L.map('mapa').setView([51.505, -0.09], 13);
+import { OpenStreetMapProvider } from 'leaflet-geosearch';
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+const lat = -33.4525756;;
+const lng = -70.6184675; 
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+const map = L.map('mapa').setView([lat, lng], 15);
+
+let markers = new L.FeatureGroup().addTo(map);
+let marker;
+
+document.addEventListener('DOMContentLoaded', () => {
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    // buscar la dirección
+    const buscador = document.querySelector('#formbuscador');
+    buscador.addEventListener('input', buscarDireccion);
+
+})
+
+
+function buscarDireccion(e) {
+    if(e.target.value.length > 8) {
+
+        // si existe un pin anterior limpiarlo
+        markers.clearLayers();
+
+        // Utilizar el provider y Geocoder
+        const provider = new OpenStreetMapProvider();
+        provider.search({query: e.target.value}).then((resultado) =>{
+
+            // mostrar el mapa
+            map.setView(resultado[0].bounds[0], 15);
+
+            // agregar el pin
+            marker = L.marker(resultado[0].bounds[0],{
+                draggable:true,
+                autoPan:true
+            })
+            .addTo(map)
+            .bindPopup(resultado[0].label)
+            .openPopup()    
+
+            // detectar movimiento del marker
+            marker.on('moveend', function(e) {
+                marker = e.target;
+                const posicion = marker.getLatLng();
+                map.panTo(new L.LatLng(posicion.lat, posicion.lng) );
+            })            
+
+            // asignar al contenedor markers
+            markers.addLayer(marker)            
+           
+        })
+
+
+    }
+}
