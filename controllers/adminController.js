962d15b83da146fa1bd7968a05ef8fa13c8c8e32
IgorNSMD <insanmartind@gmail.com>
2023-03-18 07:04:33 -0300

    Capitulo 214

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,43 +1,24 @@
 const Grupos = require('../models/Grupos');
+const Meeti = require('../models/Meeti');
 
 exports.panelAdministracion = async (req, res) => {
 
-    const grupos = await Grupos.findAll({where:{ usuarioId : req.user.id }});
+        // consultas
+        const consultas = [];
+        consultas.push(Grupos.findAll({where:{ usuarioId : req.user.id }}))
+        consultas.push( Meeti.findAll({ where : { usuarioId : req.user.id }}))
 
-    res.render('administracion', {
-        nombrePagina : 'Panel de Administracion', 
-        grupos, 
-        // meeti,
-        // anteriores,
-        // moment
-    })
+        // array destructuring
+        const [grupos, meeti] = await Promise.all(consultas);    
 
-    return;
+        res.render('administracion', {
+            nombrePagina : 'Panel de Administracion', 
+            grupos, 
+            meeti,
+            // anteriores,
+            // moment
+        })
 
-    // consultas
-    //     const consultas = [];
-    //     consultas.push( Grupos.findAll({ where: { usuarioId : req.user.id }}) );
-    //     consultas.push( Meeti.findAll({ where : { usuarioId : req.user.id, 
-    //                                               fecha : { [Op.gte] : moment(new Date()).format("YYYY-MM-DD") }
-    //                                             },
-    //                                     order : [
-    //                                         ['fecha', 'ASC']
-    //                                     ] 
-
-
-    //     }) );
-    //     consultas.push( Meeti.findAll({ where : { usuarioId : req.user.id, 
-    //                                             fecha : { [Op.lt] : moment(new Date()).format("YYYY-MM-DD") }
-    //     }}) );
-    //     // array destructuring
-    //     const [grupos, meeti, anteriores] = await Promise.all(consultas);
-        
-    //     res.render('administracion', {
-    //         nombrePagina : 'Panel de Administracion', 
-    //         grupos, 
-    //         meeti,
-    //         anteriores,
-    //         moment
-    //     })
+   
 
     }
