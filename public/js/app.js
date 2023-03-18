2e02fa22e25624dbf136a732328fabea5a70a5b3
IgorNSMD <insanmartind@gmail.com>
2023-03-17 22:11:59 -0300

    Capitulo 310

--- public/js/app.js
+++ public/js/app.js
@@ -35,29 +35,53 @@ function buscarDireccion(e) {
 
         provider.search({query: e.target.value}).then((resultado) =>{
 
-            // mostrar el mapa
-            map.setView(resultado[0].bounds[0], 15);
+            geocodeService.reverse().latlng(resultado[0].bounds[0], 15 ).run(function(error, result) {
 
-            // agregar el pin
-            marker = L.marker(resultado[0].bounds[0],{
-                draggable:true,
-                autoPan:true
+                llenarInputs(result);
+                
+                //console.log(result)
+
+                // mostrar el mapa
+                map.setView(resultado[0].bounds[0], 15);
+
+                // agregar el pin
+                marker = L.marker(resultado[0].bounds[0],{
+                    draggable:true,
+                    autoPan:true
+                })
+                .addTo(map)
+                .bindPopup(resultado[0].label)
+                .openPopup()    
+
+                // detectar movimiento del marker
+                marker.on('moveend', function(e) {
+                    marker = e.target;
+                    const posicion = marker.getLatLng();
+                    map.panTo(new L.LatLng(posicion.lat, posicion.lng) );
+
+                    // reverse geocoding, cuando el usuario reubica el pin
+                    geocodeService.reverse().latlng(posicion, 15 ).run(function(error, result) {
+
+                        llenarInputs(result);
+                    
+                        // asigna los valores al popup del marker
+                        marker.bindPopup(result.address.LongLabel);
+                    });                    
+                })                    
             })
-            .addTo(map)
-            .bindPopup(resultado[0].label)
-            .openPopup()    
-
-            // detectar movimiento del marker
-            marker.on('moveend', function(e) {
-                marker = e.target;
-                const posicion = marker.getLatLng();
-                map.panTo(new L.LatLng(posicion.lat, posicion.lng) );
-            })            
-
-        
            
         })
 
 
     }
+}
+
+
+function llenarInputs(resultado) {
+    document.querySelector('#direccion').value = resultado.address.Address || '';
+    document.querySelector('#ciudad').value = resultado.address.City || '';
+    document.querySelector('#estado').value = resultado.address.Region || '';
+    document.querySelector('#pais').value = resultado.address.CountryCode || '';
+    document.querySelector('#lat').value = resultado.latlng.lat || '';
+    document.querySelector('#lng').value = resultado.latlng.lng || '';
 }
